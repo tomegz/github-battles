8a0e5bd9f827b04b7d53a33ea96800b4b4ba1ef4
tomegz <tomaszbubala@o2.pl>
2018-05-06 00:33:20 +0200

    add fetchPopularRepos in api.js and fetch data in Popular.js

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,19 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
 import SelectLanguage from "./SelectLanguage";
-
+import { fetchPopularRepos } from "../utils/api";
 class Popular extends React.Component {
   constructor(props) {
     super(props);
     this.updateLanguage = this.updateLanguage.bind(this);
     this.state = {
-      selectedLanguage: "All"
+      selectedLanguage: "All",
+      repos: null
     };
+  }
+  componentDidMount() {
+    this.updateLanguage(this.state.selectedLanguage);
   }
   updateLanguage(lang) {
     this.setState(() => { 
       return { selectedLanguage: lang };
     });
+    fetchPopularRepos(lang)
+      .then((repos) => {
+        this.setState(() => {
+          return { repos };
+        });
+      });
   }
   render() {
     return (
