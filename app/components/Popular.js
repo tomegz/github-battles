d35e0348f43fa3d65ca5b162cb8ccba2bd0dbe21
tomegz <tomaszbubala@o2.pl>
2018-05-09 22:44:39 +0200

    finish popular repos grid, show loading on ajax request

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import SelectLanguage from "./SelectLanguage";
+import RepoGrid from "./RepoGrid";
 import { fetchPopularRepos } from "../utils/api";
 class Popular extends React.Component {
   constructor(props) {
@@ -16,7 +17,7 @@ class Popular extends React.Component {
   }
   updateLanguage(lang) {
     this.setState(() => { 
-      return { selectedLanguage: lang };
+      return { selectedLanguage: lang, repos: null };
     });
     fetchPopularRepos(lang)
       .then((repos) => {
@@ -30,6 +31,7 @@ class Popular extends React.Component {
       <div>
         <SelectLanguage selectedLanguage={this.state.selectedLanguage}
                         onSelect={this.updateLanguage} />
+        {!this.state.repos ? "Loading..." : <RepoGrid repos={this.state.repos} />}
       </div>
     );
   }
